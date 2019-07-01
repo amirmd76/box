17a21778ea9377623183eccd66288cb764db1b98
Amir Dehghan <amirmd76@gmail.com>
2019-07-01 17:54:36 +0430

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+readme = "Bounding Boxes"
+
+setup(
+    name='boxb',
+    version='1.0.0',
+    description='Utilities for 2D bounding boxes',
+    long_description=readme,
+    author='AmirMohammad Dehghan',
+    author_email='amirmd76@gmail.com',
+    url='https://github.com/amirmd76/box.git',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[]
+)
